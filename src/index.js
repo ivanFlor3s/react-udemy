d1d368daae0be513a44201a8ec6b829d1ceaaa8e
EE1955 <ivan.floresflores@externos-ar.cencosud.com>
2021-12-23 20:33:05 -0300

    cambio en scripts default

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const persona = {
+    nombre: ' Raul ',
+    apellido: ' Montanas',
+    edad: 29
+}
+console.table(persona)
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//Spread operator para en lugar de push
+const arreglo = [1,2,3,4]
+const arreglo2 = [...arreglo,5]
+const arreglo3 = arreglo.map( x=> x + 1 )
+
+console.log(arreglo)
+console.log(arreglo2)
+console.log(arreglo3);
+
+//FETCH
+const apikey = 'mz6MLpk2211YvtZTpqvkkQvHpLWkYvrg'
+
+const peticion = fetch( `https://api.giphy.com/v1/gifs/random?api_key=${apikey}` )
+    .then( resp => resp.json()) //concatena otra promesa
+    .then( ({data}) => {
+        console.log(data)
+        console.log(data.images.original)
+        const {url} = data.images.original
+
+        const img = document.createElement('img')
+        img.src = url
+        const h1tag = document.createElement('h1')
+        h1tag.textContent = 'Hola mundo'
+
+        document.body.append(h1tag)
+        document.body.append(img)
+    })
+    .catch( console.warn)
