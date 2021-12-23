f0a8d72e01e61c76589f74ce9fae578326b62393
EE1955 <ivan.floresflores@externos-ar.cencosud.com>
2021-12-23 20:56:52 -0300

    componente con fragment + estilo

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,7 +1,11 @@
 import React from "react";
 
+const saludo = 'Hola como estas Mundo?'
 const PrimeraApp = () => {
-    return <h1>Hola mundo, desde componente</h1>
+    return (<>
+                <h1>{saludo}</h1>
+                <p>Mi primera aplicacion </p>
+            </>)
 }
 
 export default PrimeraApp;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import PrimeraApp from "./PrimeraApp";
 
+import './styles.css'
+
 const divRoot = document.querySelector('#root')
 
 ReactDOM.render(<PrimeraApp/>,divRoot)
