f6ca9bdc5d3a92516ab58bc3eea3c5b96d5c06a7
EE1955 <ivan.floresflores@externos-ar.cencosud.com>
2021-12-23 21:18:10 -0300

    Prototipos

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React from "react"
+import PropTypes from 'prop-types'
 
 const saludo = 'Hola como estas?'
-const PrimeraApp = ({nombre='Vegeta'}) => {
+const PrimeraApp = ({nombre}) => {
     return (<>
                 <h1> { `${nombre}, ${saludo}`}</h1>
                 <p>Mi primera aplicacion </p>
             </>)
+}
+
+PrimeraApp.propTypes = {
+    nombre: PropTypes.string.isRequired
 }
 
 export default PrimeraApp;
